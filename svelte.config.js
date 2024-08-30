f9b37f472433285a27082be4b04ac3c2e2b9efb1
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-08-31 03:38:21 +0700

    fix: readded runes after accidentally removing them

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	compilerOptions: {
+		runes: true // Enable Runes for Svelte 5
+	}
 };
+
 export default config;
