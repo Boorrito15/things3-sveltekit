6a3f33e66f4607ba60f6db7327cc9b734af55d38
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-09-03 07:30:08 +0700

    chore: added tooltip to global components

--- src/lib/global-components.ts
+++ src/lib/global-components.ts
@@ -1,5 +1,6 @@
 // src/lib/global-components.ts
 import Checkbox from '$lib/components/ui/checkbox.svelte';
 import Tabs from '$lib/components/ui/tabs.svelte';
+import Tooltip from '$lib/components/ui/tooltip.svelte';
 
-export { Checkbox, Tabs };
+export { Checkbox, Tabs, Tooltip };
