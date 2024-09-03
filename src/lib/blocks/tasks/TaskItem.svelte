04b5eae6743a4a92320caf3ae12b544579d7ee38
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-09-03 07:32:09 +0700

    feat: added calendar, tag, checklist and flag icons to task component when expanded

--- src/lib/blocks/tasks/TaskItem.svelte
+++ src/lib/blocks/tasks/TaskItem.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Checkbox from '$lib/components/ui/checkbox.svelte';
+	import Tooltip from '$lib/components/ui/tooltip.svelte';
 
 	interface Task {
 		id: number;
@@ -15,6 +16,81 @@
 	}>();
 
 	let taskRef = $state<HTMLElement | null>(null);
+
+	const icons = {
+		calendar: {
+			svg: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#A1A3A9"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-calendar-month"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 7a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12z" />
+        <path d="M16 3v4" />
+        <path d="M8 3v4" />
+        <path d="M4 11h16" />
+        <path d="M7 14h.013" />
+        <path d="M10.01 14h.005" />
+        <path d="M13.01 14h.005" />
+        <path d="M16.015 14h.005" />
+        <path d="M13.015 17h.005" />
+        <path d="M7.01 17h.005" />
+        <path d="M10.01 17h.005" />
+      </svg>
+    `,
+			message: 'When'
+		},
+		tag: {
+			svg: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#A1A3A9"
+        stroke-width="1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-tag"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M7.5 7.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+        <path
+          d="M3 6v5.172a2 2 0 0 0 .586 1.414l7.71 7.71a2.41 2.41 0 0 0 3.408 0l5.592 -5.592a2.41 2.41 0 0 0 0 -3.408l-7.71 -7.71a2 2 0 0 0 -1.414 -.586h-5.172a3 3 0 0 0 -3 3z"
+        />
+      </svg>
+    `,
+			message: 'Tags'
+		},
+		checklist: {
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#A1A3A9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-list-check">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3.5 5.5l1.5 1.5l2.5 -2.5" />
+      <path d="M3.5 11.5l1.5 1.5l2.5 -2.5" />
+      <path d="M3.5 17.5l1.5 1.5l2.5 -2.5" />
+      <path d="M11 6l9 0" />
+      <path d="M11 12l9 0" />
+      <path d="M11 18l9 0" />
+      </svg>`,
+			message: 'Add checklist'
+		},
+		flag: {
+			svg: `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#A1A3A9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-flag-3">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16" />
+      </svg>`,
+			message: 'Deadline'
+		}
+	};
 
 	function selectTask() {
 		onSelect(task.id); // Notify the parent component to select this task
@@ -73,13 +149,20 @@
 				<p class="task-notes-collapsed">{task.notes || ''}</p>
 			{/if}
 		</div>
+		{#if task.expanded}
+			<div class="task-footer space-x-3">
+				{#each Object.entries(icons) as [key, icon]}
+					<Tooltip message={icon.message} triggerElement={icon.svg} />
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
 
 <style>
 	/* General Task Container */
 	.task-container {
-		padding: 0 0.5rem 0 1.5rem; /* Add initial padding on the left */
+		padding: 0 0.5rem 0 0.5rem; /* Add initial padding on the left */
 		border-radius: 5px;
 		overflow: hidden;
 		cursor: pointer;
@@ -97,7 +180,7 @@
 		background-color: white;
 		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
 		margin: 2rem 0;
-		padding: 1rem 0.5rem 1rem calc(1.5rem + 0.25%); /* Keep the left padding the same */
+		padding: 1rem 0.5rem 1rem calc(0.5rem + 0.25%); /* Keep the left padding the same */
 		height: fit-content;
 		width: 100%;
 	}
@@ -162,5 +245,10 @@
 		height: auto;
 		min-height: 2rem;
 		field-sizing: content;
+	}
+
+	.task-footer {
+		display: flex;
+		justify-content: end;
 	}
 </style>
--- src/routes/design-system/playground/+page.svelte
+++ src/routes/design-system/playground/+page.svelte
@@ -23,39 +23,3 @@
 		<TaskItem {task} onSelect={handleSelectTask} />
 	{/each}
 </div>
-
-<div class="wrapper">
-	<div class="inner">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi enim, venenatis non
-		facilisis sed, finibus in enim. Sed auctor enim nisl, sit amet feugiat risus blandit vitae. <br
-		/><br />
-
-		Integer dapibus tortor lorem, quis interdum erat maximus eu. Nam varius, nisl ac pharetra
-		convallis, neque ex blandit quam, sit amet condimentum lectus elit vel ligula. Donec commodo
-		porta elit quis facilisis. Sed non purus a massa convallis ultricies. Donec malesuada
-		sollicitudin dui, vel lobortis arcu rutrum ut. Curabitur lobortis ac tellus non tempor. Donec
-		nisl risus, cursus vel odio quis, convallis euismod ante. <br /><br />
-
-		Nulla lectus diam, sagittis id urna in, tincidunt facilisis sapien. Sed ante turpis, porttitor a
-		diam at, auctor lobortis sapien. Cras ornare dolor sed arcu laoreet volutpat ut tristique ipsum.
-	</div>
-</div>
-
-<style>
-	.wrapper {
-		display: grid;
-		grid-template-rows: 0fr;
-		transition: grid-template-rows 0.5s;
-		margin: 1rem;
-		padding: 1rem;
-		background: skyblue;
-	}
-
-	.wrapper:hover {
-		grid-template-rows: 1fr;
-	}
-
-	.inner {
-		overflow: hidden;
-	}
-</style>
