fe4100a37f9545958392289fcb290d7d5c2c93d7
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-09-11 10:07:40 +0700

    feat: clear task.when; hide calendar icon when task.when is set, show calendar icon when task.when is deleted, similar to Things 3

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	import type { Snippet } from 'svelte';
 	let { children }: { children: Snippet } = $props();
+	// import 'iconify-icon';
 </script>
 
 <div class="bg-[#f2f5f7] min-h-screen">
