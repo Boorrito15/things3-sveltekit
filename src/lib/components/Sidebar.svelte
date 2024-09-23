f8c990cffe810dbd6f140e3b659bd73be1634606
aldenwahsono15@icloud.com <aldenwahsono15@icloud.com>
2024-09-24 03:06:56 +0700

    feat: add Sidebar component with Today and Inbox pages; other pages to be added later

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { Page } from '@sveltejs/kit';
+	import { page } from '$app/stores'; // Import the page store from SvelteKit
+	// import { InboxIcon, CalendarIcon } from '$lib/global-icons';
+</script>
+
+<nav class="w-64 bg-[#F5F6F8] h-screen pt-2 px-2">
+	<ul class="m-0 font-medium">
+		<li class="m-0">
+			<a
+				href="/inbox"
+				class="flex items-center px-1 rounded-md {$page.url.pathname === '/inbox'
+					? 'bg-[#DEE0E6]'
+					: ''}"
+			>
+				<p>📥 Inbox</p>
+			</a>
+		</li>
+
+		<li class="mt-2">
+			<a
+				href="/today"
+				class="flex items-center px-1 rounded-md {$page.url.pathname === '/today'
+					? 'bg-[#DEE0E6]'
+					: ''}"
+			>
+				<p>⭐️ Today</p>
+			</a>
+		</li>
+
+		<li class="m-0">
+			<a
+				href="/upcoming"
+				class="flex items-center px-1 rounded-md {$page.url.pathname === '/upcoming'
+					? 'bg-[#DEE0E6]'
+					: ''}"
+			>
+				<p>📅 Upcoming</p>
+			</a>
+		</li>
+	</ul>
+</nav>
